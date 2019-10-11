d1a40320109cc31ab57c3be0ab1047db7d79af57
paracefas <emanuelclur2001@outlook.com>
2019-10-10 23:11:24 -0300

    model-interactor-app

--- ModelInteractorApplication.cpp
+++ ModelInteractorApplication.cpp
@@ -17,28 +17,10 @@ void ModelInteractorApplication::Run()
 
 IInteractor* CreateInstance(INTERACTOR_VARIANT id)
 {	
-#ifdef USE_DLL
-	HINSTANCE dll = LoadLibrary(dlls[id].file_name);
-	if (!dll) throw "Dll cannot be found";
-
-	typedef void* (*instance)(Manager&);
-	instance CreateObject = (instance)GetProcAddress(dll, "CreateInstance");
-	if (!CreateObject) throw "Dll CreateInstance cannot be found";
-#else
 	return actions[id].create();
-#endif
-
-	return NULL;
 }
 
-#ifdef USE_DLL
-	InteractorDLL dlls[] =
-	{
-		{ CONSOLE_APPLICATION,  "CoutInteractor.dll" }
-	};
-#else
-	InteractorAction actions[] =
-	{
-		{ CONSOLE_APPLICATION, CreateCoutInteractor }
-	};
-#endif
+InteractorAction actions[] =
+{
+	{ CONSOLE_APPLICATION, CreateCoutInteractor }
+};
